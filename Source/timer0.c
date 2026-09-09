6411557eb27127d74d4a4425af7d8dc59854522b
Amr Farag <amrofarag222@gmail.com>
2026-09-09 16:29:32 +0300

    Update timer0.c

--- Source/timer0.c
+++ Source/timer0.c
@@ -1,24 +1,71 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
+#include "std_type.h"
+#include "bit_math.h"
 #include "DIO.h"
 #include "timer0.h"
 
-void timer0_init(void) {
-    // 1. Normal Mode (Overflow Mode)
-    CLR_BIT(TCCR0, WGM00);
-    CLR_BIT(TCCR0, WGM01);
+void timer0_init(u8 mode) {
+    switch (mode) {
+        case TIMER0_NORMAL_MODE: // Normal Mode (Overflow Mode)
+            CLR_BIT(TCCR0, WGM00);
+            CLR_BIT(TCCR0, WGM01);
 
-    // 2. Prescaler 64
-    SET_BIT(TCCR0, CS00);
-    SET_BIT(TCCR0, CS01);
-    CLR_BIT(TCCR0, CS02);
+            // Prescaler = 8 time =1us 
+            CLR_BIT(TCCR0, CS00);
+            SET_BIT(TCCR0, CS01);
+            CLR_BIT(TCCR0, CS02);
 
-    // 3. Initial value
-    TCNT0 = 0;
+            TCNT0 = 0;
 
-    // 4. Enable Timer0 Overflow Interrupt (TIMSK)
-    SET_BIT(TIMSK, TOIE0);
+            // Enable Overflow Interrupt
+            SET_BIT(TIMSK, TOIE0);
 
-    // 5. Enable Global Interrupt
-    sei();
+            // Enable Global Interrupt
+            sei();
+            break;
+
+        case TIMER0_CTC_MODE: // CTC Mode (Clear Timer on Compare Match)
+            CLR_BIT(TCCR0, WGM00);
+            SET_BIT(TCCR0, WGM01);
+
+            // Initial OCR0 Value
+            OCR0 = 249; //page 80
+
+            // Prescaler = 8
+            CLR_BIT(TCCR0, CS00);
+            SET_BIT(TCCR0, CS01);
+            CLR_BIT(TCCR0, CS02);
+
+            TCNT0 = 0;//page 80
+
+            // Enable Compare Match Interrupt
+            SET_BIT(TIMSK, OCIE0);
+
+            // Enable Global Interrupt
+            sei();
+            break;
+
+        case FAST_PWM_MODE: //fast pwm
+            SET_BIT(TCCR0, WGM00);
+            SET_BIT(TCCR0, WGM01);
+            //Compare Output Mode, Fast PWM Mode ,Clear OC0 on compare match, set OC0 at TOP
+            CLR_BIT(TCCR0, COM00);
+            SET_BIT(TCCR0, COM01);
+
+            CLR_BIT(TCCR0, CS00);
+            SET_BIT(TCCR0, CS01);
+            CLR_BIT(TCCR0, CS02);
+            
+            OCR0 = 249; //page 80
+
+            SET_BIT(TIMSK, OCIE0);
+
+            sei();
+            break;
+    }
+}
+
+void timer0_setCompareValue(u8 compare_value) {
+    OCR0 = compare_value;
 }
